9792441f822b24c4b4c0703bbf97e0a9d8c308aa
Riley Peterson <rileypeterson21@gmail.com>
2022-02-24 13:50:32 -0800

    Convert markdown script

---
+++ mk_output.sh
@@ -0,0 +1,7 @@
+base_dir="/Users/riley/PycharmProjects/rileypeterson.github.io"
+chapter="chapter2"
+file="notes"
+jupyter nbconvert --NbConvertApp.output_files_dir="$base_dir/assets/images/ml-book/$chapter" --to markdown $chapter/$file.ipynb
+mv $chapter/$file.md $base_dir/_projects/ml-book/$chapter/$file.md
+# Replace absolute paths to images with relative ones
+python -c "f = open('$base_dir/_projects/ml-book/$chapter/$file.md'); o = f.read().replace('![png]($base_dir', '![png]('); f = open('$base_dir/_projects/ml-book/$chapter/$file.md', 'w'); f.write(o); f.close();"
