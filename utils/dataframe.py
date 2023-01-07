d2d992c8c4dcd407765be3b1310f54a5dfd26644
Riley Peterson <rileypeterson21@gmail.com>
2023-01-07 13:26:14 -0800

    tar thingy

--- utils/dataframe.py
+++ utils/dataframe.py
@@ -29,25 +29,25 @@ class DataFrame(pd.DataFrame):
         r = urllib.request.urlopen(url)
         with tempfile.TemporaryDirectory() as d:
             with tarfile.open(fileobj=r, mode="r:gz") as tf:
+
                 def is_within_directory(directory, target):
-                    
+
                     abs_directory = os.path.abspath(directory)
                     abs_target = os.path.abspath(target)
-                
+
                     prefix = os.path.commonprefix([abs_directory, abs_target])
-                    
+
                     return prefix == abs_directory
-                
+
                 def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-                
+
                     for member in tar.getmembers():
                         member_path = os.path.join(path, member.name)
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
-                
-                    tar.extractall(path, members, numeric_owner=numeric_owner) 
-                    
-                
+
+                    tar.extractall(path, members, numeric_owner=numeric_owner)
+
                 safe_extract(tf, path=d)
                 name = tf.getnames()[0]
             df = pd.read_csv(os.path.join(d, name))
